5d5fe24280d8066dcdc0dee3ecd80e1f4ec3db3b
LautaroDeLaPuente <66932866+LautaroDeLaPuente@users.noreply.github.com>
2020-11-07 10:33:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
   prefix: "-",
   color: "#00e059",
-  token: "NzEwMjM2NzMzMjExNjcyNjkw.XuUwJA.i9Fw_PpVh7GueRA28iEdItgskL0",
+  token: "token here lol",
   weez: ""
  // El prefix de su bot para indentificar sus comandos.
 
